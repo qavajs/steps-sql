10c48dfcdbfc5a0f4308a236d8dbafc2277439aa
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-12-20 13:58:26 +0300

    updated connection error property name

--- src/clients/MySQLClient.ts
+++ src/clients/MySQLClient.ts
@@ -5,19 +5,19 @@ export class MySQLClient implements DBClient {
 
     public connection: Connection | null = null;
     private readonly config: any;
-    private readonly connectError = new Error('Connection is not established! Call connect method!');
+    private readonly CONNECTION_ERROR = new Error('Connection is not established! Call connect method!');
 
     constructor(config: any) {
         this.config = config;
     }
 
     async close(): Promise<void> {
-        if (!this.connection) throw this.connectError;
+        if (!this.connection) throw this.CONNECTION_ERROR;
         await this.connection.end();
     }
 
     async execute(query: string): Promise<Array<Array<any>>> {
-        if (!this.connection) throw this.connectError;
+        if (!this.connection) throw this.CONNECTION_ERROR;
         const [results] = await this.connection.query(query);
         return results as any
     }
