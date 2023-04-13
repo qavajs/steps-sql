209ad6bf4f524e24ce6220a585ff56468886d589
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-04-13 20:30:22 +0300

    added lazy init for db (#10)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+implemented lazy way to connect
+
 ## 0.0.2
 implemented pg client
 
--- src/clients/DBClient.ts
+++ src/clients/DBClient.ts
@@ -1,4 +1,5 @@
 export default interface DBClient {
+    connection: any;
     /**
      * Establish connection
      */
